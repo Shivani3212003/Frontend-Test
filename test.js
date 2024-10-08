3a2a97c12507aa1b9aada5de586d4d5dfd9cece8
Shivani1321 <shivanibhujbal321@gmail.com>
2024-10-08 13:37:56 +0530

    msg

---
+++ test.js
@@ -0,0 +1,27 @@
+function doMultipllication(n){
+    for(i=1;i<=10;i++){
+        let result=i*n;
+        console.log(result);
+        
+    }
+}
+doMultipllication(3);
+
+function doCal(c){
+    let Fahernheit=((c*9/5)+32) ;
+    console.log("celsius: "+Fahernheit);
+    
+}
+doCal(10);
+
+function doCalculation(n){
+    if(n>0){
+        console.log("number is positive");
+        
+    }else{
+        console.log("number is negitive");
+        
+    }
+}
+doCalculation(12);
+
